0f9a4cdb57d1779a9dc0429539ff9b01d3e300de
drchand-007 <dr.rcn007.gm@gmail.com>
2025-11-15 11:51:16 +0530

    Fixing Shorter receipt ID issue

--- api/createOrder.js
+++ api/createOrder.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
     const options = {
       amount: amount,
       currency: "INR",
-      receipt: `receipt_user_${uid}_${Date.now().toString(36)}`, // <-- THIS IS THE FIX
+      receipt: `${uid}_${Date.now().toString(36)}`,
     };
 
     const order = await razorpay.orders.create(options);
